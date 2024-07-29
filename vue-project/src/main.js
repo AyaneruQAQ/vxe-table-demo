e8bf4fc99bd0a842fb9d0fee333d7998ddb2c0d8
zhikai.luo <zhikai.luo@zkh.com>
2024-07-29 18:08:15 +0800

    vxe demo

---
+++ vue-project/src/main.js
@@ -0,0 +1,8 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import VXETable from 'vxe-table';
+import 'vxe-table/lib/style.css';
+
+createApp(App).use(VXETable).mount('#app')
